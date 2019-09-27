a7aa6db534b8c15b7d27564a9fe89893ab4867e4
gabrielpoca <gabrielpoca@users.noreply.github.com>
2019-09-27 10:22:58 +0000

    Automated deployment: Fri Sep 27 10:22:58 UTC 2019 76b81efb6e0aa13505db5f938c9620236767d319

---
+++ precache-manifest.feaf014e76b6afb597a8c47bd0143b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786ecb7b290a261c4c3aecd1260e3618",
+    "url": "/08b1b4c3af9fd96811d2.worker.js"
+  },
+  {
+    "revision": "3e87ed7c68e47979b70ebdc6348d7fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbee3f79f38be0506702",
+    "url": "/static/js/2.0d1a789f.chunk.js"
+  },
+  {
+    "revision": "03a7c2db33921f9cc688",
+    "url": "/static/js/main.e1cf7916.chunk.js"
+  },
+  {
+    "revision": "f4fabdf54e7f78079802",
+    "url": "/static/js/runtime~main.e0422c1d.js"
+  },
+  {
+    "revision": "f302813be19295a34400d9348db35f76",
+    "url": "/static/media/face.f302813b.png"
+  },
+  {
+    "revision": "9227568e0d84a15b5b3597a6a1ee7c85",
+    "url": "/static/media/mouth.9227568e.png"
+  }
+]);
